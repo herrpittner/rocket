776f256a3f311159930a3a99a04a8586c0bb95ff
Petr Pittner <herr.pittner@gmail.com>
2020-02-08 21:48:02 +0100

    added and improved telemetry data

--- scripts/code_1.js
+++ scripts/code_1.js
@@ -1,9 +1,10 @@
-var myInterval = 20;
+const myInterval = 20;
 var myT = 0;
-var myVel = 0;
+var myHorVel = 0;
+var myHorDist = 0;
 var myVerDist = 0;
 var myHorDist = 0;
-var g = -9.81;
+const g = -9.81;
 
 
 function RunEngine() {
@@ -13,15 +14,18 @@ function RunEngine() {
 function EngineTimeTick() {
 	// do calculations on Physical quantities
 	myT = (myT * 1000 + myInterval) / 1000;
-	myVel = myT * g;
+	myHorVel = 50;
+	myVerVel = myT * g;
 	myVerDist = 1/2 * g * myT * myT;
-	myHorDist = 50 * myT;
+	myHorDist = myHorVel * myT;
 
 
 	// print it to telemetry table
 	document.getElementById("time").innerHTML = myT.toFixed(3) + " s";
-	document.getElementById("velocity").innerHTML = myVel.toFixed(3)+" m/s";
-	document.getElementById("distance").innerHTML = myVerDist.toFixed(3)+ " m";
+	document.getElementById("Vvel").innerHTML = myVerVel.toFixed(3)+" m/s";
+	document.getElementById("Vdist").innerHTML = myVerDist.toFixed(3)+ " m";
+	document.getElementById("Hvel").innerHTML = myHorVel.toFixed(3)+" m/s";
+	document.getElementById("Hdist").innerHTML = myHorDist.toFixed(3)+ " m";
 
 	// move the driver
 	document.getElementById("driver").style.bottom = 440 + myVerDist + "px";
