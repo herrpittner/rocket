747ae6dd0dc4f0e08f868753c5bf8842dee71462
Petr Pittner <herr.pittner@gmail.com>
2020-02-08 16:15:35 +0100

    driver moves!

--- scripts/code_1.js
+++ scripts/code_1.js
@@ -2,22 +2,24 @@ var myInterval = 20;
 var myT = 0;
 var myVel = 0;
 var myDist = 0;
-var Output1;
-var Output2;
-var Output3;
+
 
 function RunEngine() {
-	var myVar = setInterval("EngineTime()", myInterval);
+	var myVar = setInterval("EngineTimeTick()", myInterval);
 }
 
-function EngineTime() {
+function EngineTimeTick() {
+	// do calculations on Physical quantities
 	myT = (myT * 1000 + myInterval) / 1000;
 	myVel = myT * 9.81;
 	myDist = 1/2 * 9.81 * myT * myT;
 
 
-
+	// print it to telemetry table
 	document.getElementById("time").innerHTML = myT.toFixed(3) + " s";
 	document.getElementById("velocity").innerHTML = myVel.toFixed(3)+" m/s";
 	document.getElementById("distance").innerHTML = myDist.toFixed(3)+ " m";
+
+	// move the driver
+	document.getElementById("driver").style.top = myDist + "px"
 }
