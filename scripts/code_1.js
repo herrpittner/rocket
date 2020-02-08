778765b8b0523357d24de58dba29f2d91cdd6f62
Petr Pittner <herr.pittner@gmail.com>
2020-02-08 23:46:39 +0100

    added altitude telemetry; added engine stop on 0 altitude; vert position based on altitude;

--- scripts/code_1.js
+++ scripts/code_1.js
@@ -1,4 +1,4 @@
-const myInterval = 20;
+const myInterval = 20; //ms
 var myT = 0;
 var myHorVel = 0;
 var myHorDist = 0;
@@ -6,24 +6,25 @@ var myVerDist = 0;
 var myHorDist = 0;
 var myPitch = 0;
 const g = -9.81;
-var  myVel = 0;
-
+var myVel = 0;
+var myAltitude = 0;
+var initAltitude = 50;
+var myTick = null;
 
 function RunEngine() {
-	var myVar = setInterval("EngineTimeTick()", myInterval);
+	myTick = setInterval("EngineTimeTick()", myInterval);
 }
 
 function EngineTimeTick() {
-	// do calculations on Physical quantities
+	// do calculations on physical quantities
 	myT = (myT * 1000 + myInterval) / 1000;
 	myHorVel = 50;
 	myVerVel = myT * g;
-	myVerDist = 1/2 * g * myT * myT;
+	myVerDist = 1/2 * myVerVel * myT;
 	myHorDist = myHorVel * myT;
 	myPitch = Math.atan(myVerVel/myHorVel) * 180 / Math.PI;
-	// myPitch = Math.tan(myVerVel/myHorVel) * 180 / Math.PI;
 	myVel = Math.sqrt(myVerVel*myVerVel + myHorVel*myHorVel);
-
+	myAltitude = initAltitude + myVerDist;
 
 	// print it to telemetry table
 	document.getElementById("time").innerHTML = myT.toFixed(3) + " s";
@@ -33,8 +34,14 @@ function EngineTimeTick() {
 	document.getElementById("Hdist").innerHTML = myHorDist.toFixed(3)+ " m";
 	document.getElementById("Vel").innerHTML = myVel.toFixed(3)+ " m/s";
 	document.getElementById("Pitch").innerHTML = myPitch.toFixed(3)+ " deg";
+	document.getElementById("Alt").innerHTML = myAltitude.toFixed(3)+ " m";
 
 	// move the driver
-	document.getElementById("driver").style.bottom = 440 + myVerDist + "px";
+	document.getElementById("driver").style.bottom = myAltitude + "px";
 	document.getElementById("driver").style.left = myHorDist + "px"
+
+	// stop engine when altitude is 0
+	if (myAltitude < 0) {
+		clearInterval(myTick);
+	}
 }
