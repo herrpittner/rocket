b577a19515f3d7da3cd15cc0cd0476bfb71464aa
Petr Pittner <herr.pittner@gmail.com>
2020-02-09 00:10:46 +0100

    changed interval; added auto initial position set;

--- scripts/code_1.js
+++ scripts/code_1.js
@@ -1,5 +1,5 @@
-const myInterval = 20; //ms
-var myT = 0;
+const myInterval = 10; //ms
+var myTime = 0;
 var myHorVel = 0;
 var myHorDist = 0;
 var myVerDist = 0;
@@ -9,25 +9,33 @@ const g = -9.81;
 var myVel = 0;
 var myAltitude = 0;
 var initAltitude = 50;
+var initHorVel = 50;
+var initVerVel = 50;
 var myTick = null;
+
+window.onload = SetInitDriverPosition;
 
 function RunEngine() {
 	myTick = setInterval("EngineTimeTick()", myInterval);
 }
 
+function SetInitDriverPosition() {
+	document.getElementById("driver").style.bottom = initAltitude + "px";
+}
+
 function EngineTimeTick() {
 	// do calculations on physical quantities
-	myT = (myT * 1000 + myInterval) / 1000;
-	myHorVel = 50;
-	myVerVel = myT * g;
-	myVerDist = 1/2 * myVerVel * myT;
-	myHorDist = myHorVel * myT;
+	myTime = (myTime * 1000 + myInterval) / 1000;
+	myHorVel = initHorVel;
+	myVerVel = initVerVel + myTime * g;
+	myVerDist = 1/2 * myVerVel * myTime;
+	myHorDist = myHorVel * myTime;
 	myPitch = Math.atan(myVerVel/myHorVel) * 180 / Math.PI;
 	myVel = Math.sqrt(myVerVel*myVerVel + myHorVel*myHorVel);
 	myAltitude = initAltitude + myVerDist;
 
 	// print it to telemetry table
-	document.getElementById("time").innerHTML = myT.toFixed(3) + " s";
+	document.getElementById("time").innerHTML = myTime.toFixed(3) + " s";
 	document.getElementById("Vvel").innerHTML = myVerVel.toFixed(3)+" m/s";
 	document.getElementById("Vdist").innerHTML = myVerDist.toFixed(3)+ " m";
 	document.getElementById("Hvel").innerHTML = myHorVel.toFixed(3)+" m/s";
